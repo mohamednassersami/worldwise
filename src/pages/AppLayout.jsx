b444259d0e6ac330c7a95eb3041a4d09834b7004
mohamed <mohamed97@gmail.com>
2023-09-05 17:06:55 +0300

    worldwise with protected Route (Auth) using context and routes

---
+++ src/pages/AppLayout.jsx
@@ -0,0 +1,16 @@
+import Map from "../components/Map";
+import User from "../components/User";
+import Sidebar from "../components/sidebar";
+import styles from "./AppLayout.module.css";
+
+function AppLayout() {
+  return (
+    <div className={styles.app}>
+      <Sidebar />
+      <Map />
+      <User />
+    </div>
+  );
+}
+
+export default AppLayout;
